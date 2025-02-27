064ab218eb179207f154f9599951e4c3b4142617
Jasper Flick <jrjflick@catlikecoding.com>
2025-02-27 16:11:34 +0100

    Added part 6.

---
+++ 6-multiple-maps/map.gd
@@ -0,0 +1,35 @@
+extends Node
+
+var registered_detector_count := 0
+var valid_detector_count := 0
+
+
+func _ready() -> void:
+	for child_node in get_children():
+		var detector := child_node as Detector
+		if detector:
+			registered_detector_count += 1
+			detector.validity_changed.connect(_on_detector_validity_changed)
+
+
+func _on_detector_validity_changed(valid: bool):
+	if valid:
+		valid_detector_count += 1
+		if valid_detector_count == registered_detector_count:
+			load_next_map.call_deferred()
+	else:
+		valid_detector_count -= 1
+
+
+func load_next_map():
+	var current_map_path := get_tree().current_scene.scene_file_path
+	var split_path := current_map_path.split(".")
+	var next_map_number := split_path[1].to_int() + 1
+	split_path[1] = str(next_map_number).pad_zeros(3)
+	var next_map_path = ".".join(split_path)
+	
+	if not ResourceLoader.exists(next_map_path):
+		split_path[1] = "001"
+		next_map_path = ".".join(split_path)
+		
+	get_tree().change_scene_to_file(next_map_path)
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [1.5.0] - 2024-10-24
+## [1.6.0] - 2025-02-27
+
+### Added
+
+- *6-multiple-maps*
+
+## [1.5.0] - 2024-12-24
 
 ### Added
 
